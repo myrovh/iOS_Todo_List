267b94550bdb777fba2688716b22fbcb3cb70c79
myrovh <myrovh@yandex.com>
2016-08-01 14:43:20 +1000

    incomplete commit

---
+++ Reminder List/AddReminderController.swift
@@ -0,0 +1,15 @@
+//
+//  AddReminderController.swift
+//  Reminder List
+//
+//  Created by Dyson Talbot Sunderland Hamilton on 1/08/2016.
+//  Copyright © 2016 myrovh. All rights reserved.
+//
+
+import UIKit
+
+class AddReminderController: UIViewController {
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var descriptionText: UITextField!
+
+}
---
+++ ReminderListController.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderListController.swift
+//  Reminder List
+//
+//  Created by Dyson Talbot Sunderland Hamilton on 1/08/2016.
+//  Copyright © 2016 myrovh. All rights reserved.
+//
+
+import UIKit
+
+class ReminderListController: UITableViewController {
+
+}
